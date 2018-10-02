3609a381c435484bd83d69919ab1c673b4544550
vinjerdim <marjer17dim@gmail.com>
2018-10-02 17:48:29 +0700

    add script to load json

--- index.js
+++ index.js
@@ -5,6 +5,56 @@ const bodyParser     = require('body-parser');
 const port           = 8123;
 const app            = express();
 
+var connInfo = {
+    host: 'localhost',
+    port: 8000,
+    user: 'hospital',
+    password: 'Hospital1234'
+};
+
+const marklogic = require('marklogic');
+const db = marklogic.createDatabaseClient(connInfo);
+
+const patients = require("./patient.json");
+patients.forEach(patient => {
+    var record = {
+        'uri' : '/patient/' + patient.patient_id,
+        'content' : patient,
+        'collections' : ['patient']
+    }
+    db.documents.write(record).result( 
+        function(response) {
+            console.log('Loaded the following documents:');
+            response.documents.forEach(function(document) {
+                console.log(document);
+            });
+        }, 
+        function(error) {
+            console.log(JSON.stringify(error, null, 2));
+        }
+    );
+});
+
+const doctors = require("./doctor.json");
+doctors.forEach(doctor => {
+    var record = {
+        'uri' : '/doctor/' + doctor.doctor_id,
+        'content' : doctor,
+        'collections' : ['doctor']
+    }
+    db.documents.write(record).result( 
+        function(response) {
+            console.log('Loaded the following documents:');
+            response.documents.forEach(function(document) {
+                console.log(document);
+            });
+        }, 
+        function(error) {
+            console.log(JSON.stringify(error, null, 2));
+        }
+    );
+});
+
 app.use(bodyParser.json());         // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
@@ -13,6 +63,8 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 app.get('/', function(request, response) {
     response.send('Temporal Hospital REST API');
 });
+
+app.post('/insert')
 
 app.listen(port, () => {
     console.log('Temporal Hospital REST API is running on ' + port);
